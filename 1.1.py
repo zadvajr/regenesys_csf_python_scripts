83ed4746587a53e44609326263a670946d53371d
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-03-14 00:16:55 +0100

    1.1 python program to do arithmetic

---
+++ 1.1.py
@@ -0,0 +1,37 @@
+# Python Program for Arithmetic Operations:
+
+# Store input numbers:
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Subtract two numbers
+min = float(num1) - float(num2)
+
+# Multiply two numbers
+mul = float(num1) * float(num2)
+
+# Divide two numbers
+div = float(num1) / float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Display the subtraction
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min))
+
+# Display the multiplication
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))
+
+# Display the division
+print('The division of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Output:
+# Enter first number: 20
+# Enter second number: 10
+# The sum of 20 and 10 is 30.0
+# The subtraction of 20 and 10 is 10.0
+# The multiplication of 20 and 10 is 200.0
+# The division of 20 and 10 is 30.0
