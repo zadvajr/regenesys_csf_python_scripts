695e4602746ad7f6321593c8f2819b69bb15a506
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-03-16 00:46:14 +0100

    2.0 area of triangle

---
+++ 2.0.py
@@ -0,0 +1,13 @@
+# Python Program for Area of Triangle:
+b = float(input('Enter the base: '))
+h = float(input('Enter the height: '))
+
+# Calculate the area
+area = 1/2 * b * h
+
+print('The area of the triangle is %0.2f' % area)
+
+# Output:
+# Enter the base: 10
+# Enter the height: 5
+# The area of the triangle is 25.00
